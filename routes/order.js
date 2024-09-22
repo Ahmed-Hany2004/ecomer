61173aaeaab95989846050fa4f42cbff8c1fed88
Ahmed <ahmedhanycc053@gmail.com>
2024-09-22 14:42:51 +0300

    first commit

--- routes/order.js
+++ routes/order.js
@@ -126,7 +126,7 @@ router.post("/", async (req, res) => {
         test = await userorders.findOne({"_id":new ObjectId(req.body.Customer)})
 
         if(test){
-         await userorders.updateOne({"_id":new ObjectId(req.body.Customer)},{"lastOrder":Date.now()})
+         await userorders.updateOne({"Customer":new ObjectId(req.body.Customer)},{"lastOrder":Date.now()})
         }
         else{
          await userorders.insertOne({
